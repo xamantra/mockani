3b92964b3e0078b9a5da4dce6af4e3519da4eff6
Charlou Aredidon <xamantra.dev@gmail.com>
2022-11-27 08:59:27 +0800

    added fonts for the kana/kanji (not sure if it works)

--- lib/src/screens/review.dart
+++ lib/src/screens/review.dart
@@ -146,6 +146,8 @@ class _ReviewScreenState extends State<ReviewScreen> {
                                       color: Colors.white,
                                       fontWeight: FontWeight.w500,
                                       fontSize: 128,
+                                      fontFamily:
+                                          'Hiragino Kaku Gothic Pro", "Meiryo", "Source Han Sans Japanese", "NotoSansCJK", "TakaoPGothic", "Yu Gothic", "ヒラギノ角ゴ Pro W3", "メイリオ", "Osaka", "MS PGothic", "ＭＳ Ｐゴシック", "Noto Sans JP", "PingFang SC", "Noto Sans SC", sans-serif',
                                     ),
                                   ),
                           ),
